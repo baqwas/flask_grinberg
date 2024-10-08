109857abab1450c4ce87b637bf916432c6f125f1
baqwas <baqwas@gmail.com>
2024-10-08 13:13:44 -0500

    SQLite preparation using SQLAlchemy

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')  # SQLite record
